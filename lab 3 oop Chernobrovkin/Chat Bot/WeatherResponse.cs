4c8377ac673101f87db7e14e1c2f952a5eae8a81
Roboveg <55119318+Roboveg@users.noreply.github.com>
2020-06-25 01:13:49 +0900

    Add files via upload

---
+++ lab 3 oop Chernobrovkin/Chat Bot/WeatherResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_Bot
+{
+    public class WeatherResponse
+    {
+        public TemperatureInfo Main { get; set; }
+
+        public string Name { get; set; }
+
+        public WindInfo Wind { get; set; }
+    }
+}
